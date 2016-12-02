df5299665457e7e2c61f866ce28e190430948d3d
wostlund <wostlund@stuy.edu>
2016-12-01 19:14:29 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
+#include <limits.h>
 #include <fcntl.h>
 
 #define MAXCOMMANDS 50
@@ -15,15 +16,15 @@ int main () {
 
 	while(1) //inside of while loop reads one-line instruction, splits it by ';', and gives the pieces to singleCommand function
 	{
-	        if(reply == -1){return 0;}
-		printf("What would you like to do? ");
-		char *a = (char *) calloc(1, 256);
-		fgets(a, 255, stdin);
 
-		char* ans[MAXCOMMANDS];
-		*(strchr(a, '\n')) = 0;
-
-		int i = 0;
+        if(reply == -1){return 0;}
+        printf("What would you like to do? ");
+        char *a = (char *) calloc(1, 256);
+        fgets(a, 255, stdin);
+        
+        char* ans[MAXCOMMANDS];
+        *(strchr(a, '\n')) = 0;
+        int i = 0;
 		while(a)
 		{
 			char *s = strsep(&a, ";");
@@ -35,37 +36,18 @@ int main () {
 		int n = 0;
 		for(; n < i; n++)
 		{
-			int f = fork();
-			int status;
-			if(reply == -1){ exit(1); return 0;}
-			reply = wait(&status);
-			if(f == 0) {
-			  singleCommand(ans[n]);
-			}
-      if(WIFSIGNALED(status))
-      {
+            singleCommand(ans[n]);
 
-        //printf("cd signal sent successfully\n");
-        char* dir = (char *) calloc(1, 50);
-        int fd = open("auxfile.txt", O_RDONLY);
-        read(fd, dir, 49);
-        close(fd);
-
-        //have to deal with special case of dir being empty string
-        chdir(dir);
-        free(dir);
-      }
 		}
 
 
 	}
 
 	return 0;
-
 }
 
-int singleCommand(char* a) {
 
+int singleCommand(char* a) {
 	char *ans[50];
 	char *n = calloc(1, strlen(a));
 	int i=0;
@@ -100,12 +82,15 @@ int singleCommand(char* a) {
 
   	if(strcmp(ans[0], "cd") == 0)
   	{
-      //printf("%s\n", ans[1]);
-
-      //prints ans[1] to an auxiliary file. 
-      //then uses exit(3) and WIFEXITED to inform parent process that cd was used
-      //finally, parent uses aux file to use chdir
-      if(ans[1] == NULL)
+        /*char *dir = calloc(PATH_MAX + 1, 1);
+        char buff[PATH_MAX + 1];
+        dir = getcwd(buff, PATH_MAX);
+        *(dir + strlen(dir)) = '/';
+        char *newdir = strcat(dir, ans[1]);*/
+        printf("%s\n", ans[1]);
+        chdir(ans[1]);
+        return 1;
+    }/*if(ans[1] == NULL)
       {
         //printf("clearing\n");
         int fd = open("auxfile.txt", O_CREAT | O_TRUNC, 0644);
@@ -118,18 +103,24 @@ int singleCommand(char* a) {
         write(fd, ans[1], strlen(ans[1]));
         close(fd);
         kill(getpid(), SIGTERM);
-      }
-  	}
+      }*/
   	if(strcmp(ans[0], "exit") == 0)
   	{
-	  kill(getppid(), SIGUSR1);
-	  exit(0);
-  	}
-
-  	int ret = execvp(ans[0], ans);
-  	if(ret) printf("-bash: %s: command not found\n", ans[0]); //maybe we should use the exit function here as well
+        exit(0);
+  	}    
+    int f = fork();
+    int status;
+    if(f == 0) {
+        int ret = execvp(ans[0], ans);
+  	    if(ret) printf("-bash: %s: command not found\n", ans[0]);
+        exit(0); 
+    }else{
+        reply = wait(&status);
+    }
+//maybe we should use the exit function here as well
                                                             //otherwise, the forked version could be the one continuing
-  	exit(0); //should exit from forked version in case execvp did not work
+    //should exit from forked version in case execvp did not work
   	return 1;
 
 }
+
